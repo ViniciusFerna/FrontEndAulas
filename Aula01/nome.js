86dbe43cee681edb7555d691fa87780faa07b483
ViniciusFerna <vinicius020206@gmail.com>
2025-02-10 20:00:46 -0300

    Aula01

---
+++ Aula01/nome.js
@@ -0,0 +1,36 @@
+let nome = "João";
+
+console.log(nome);
+/* ------------ */
+var num1 = 10;
+var num2 = 6;
+
+console.log(num1 + num2);
+/* -------------------- */
+var idade = 19;
+var cidade = "Cotia";
+var gostaDePizza = true;
+
+console.log(idade);
+console.log(cidade);
+console.log(gostaDePizza);
+/* ------------------------- */
+var a = 10;
+var b = 30;
+
+var c = a;
+a = b;
+b = c;
+
+console.log("a: " + a + " b: " + b);
+/* ------------------------------ */
+let primeiroNome = "Vinicius";
+let sobrenome = "Fernandes";
+
+console.log(primeiroNome + " " + sobrenome);
+/* -------------------------------------- */
+var numString = '42';
+
+parseInt(numString);
+
+console.log(numString);
