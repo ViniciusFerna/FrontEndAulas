098cfaf3d264b07e5bdf76e549198dfa5ed08179
ViniciusFerna <vinicius020206@gmail.com>
2025-02-14 20:15:11 -0300

    Aula03 exercicios

--- Aula03/arrayFuncao.js
+++ Aula03/arrayFuncao.js
@@ -27,3 +27,27 @@ console.log(existeAlgumNumeroMaiorQue100);
 // EVERY - Verifica se todos atendem a condição
 let todosOsNumerosSaoMaioresQue9 = numeros.every((i) => i > 9);
 console.log(todosOsNumerosSaoMaioresQue9);
+
+// Exercicio 1
+let exercicio1 = [1, 2, 3, 4, 5];
+console.log(exercicio1.map((i) => i * 2));
+
+// Exercicio 2
+let exercicio2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(exercicio2.filter((i) => i > 5));
+
+// Exercicio 3
+let exercicio3 = [10, 25, 47, 58, 62, 79];
+console.log(exercicio3.find((i) => i > 50));
+
+// Exercicio 4
+let exercicio4 = [10, -5, 7, 0, -3];
+console.log(exercicio4.some((i) => i < 0));
+
+// Exercicio 5
+let exercicio5 = [1, 2, 3, 4, 5];
+console.log(exercicio5.every((i) => i > 2));
+
+// Exercicio 6
+let exercicio6 = [1, 3, 5, 7, 9, 10];
+console.log(exercicio6.includes(7));
