39cf3da77884e6c1a3f68776f95e7b8a10f39b89
ViniciusFerna <vinicius020206@gmail.com>
2025-02-13 21:33:38 -0300

    Ajustando responsividade

--- Aula02/lista-de-compras/script.js
+++ Aula02/lista-de-compras/script.js
@@ -6,7 +6,7 @@ function adicionarItem() {
 
     if (inputItem.value.trim() === "") {
         return;
-    }
+    };
 
     let item = {
         id: Date.now(),
@@ -24,14 +24,14 @@ function adicionarItem() {
     salvarDados();
 
     
-}
+};
 
 
 function limparLista() {
     listaDeCompras = [];
     atualizarInterface();
     salvarDados();
-}
+};
 
 
 function atualizarInterface() {
@@ -64,7 +64,7 @@ function toggleItem(id) {
     const item = listaDeCompras.find((item) => item.id === id);
     item.comprado = !item.comprado;
     salvarDados();
-}
+};
 
 document.getElementById("item").addEventListener("keypress", function(e) {
     if(e.key === "Enter") {
@@ -82,14 +82,14 @@ function carregarDados() {
     const dados = localStorage.getItem('lista_de_compras');
 
     if(dados){
-        listaDeCompras = JSON.parse(dados) // O parse destransforma uma string e a transforma em um objeto
+        listaDeCompras = JSON.parse(dados); // O parse destransforma uma string e a transforma em um objeto
         atualizarInterface();
-    }
-}
+    };
+};
 
 function salvarDados() {
     /* sessionStorage.setItem('testeSessionStorage', "hello Session"); // Continua lá até fechar o navegador */
     localStorage.setItem('lista_de_compras', JSON.stringify(listaDeCompras)); // Continua lá até alguém apagar \ O JSON stringfy transforma tudo entre o parenteses em uma string
-}
+};
 
 carregarDados();
